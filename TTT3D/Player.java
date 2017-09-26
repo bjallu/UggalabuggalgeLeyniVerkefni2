1c9b6b0537c0e1efa45dac967858dcc296ebdbee
bjallu <bjartur@kth.se>
2017-09-26 17:43:45 +0200

    new heuristic

--- TTT3D/Player.java
+++ TTT3D/Player.java
@@ -10,9 +10,7 @@ public class Player {
 	 * 
 	 */
 	
-    private final int MAX_DEPTH = 3;
-    private final int[] Xweights = {0,1,10,100,1000};
-    private final int[] Oweights = {0,1,9,95,950};
+    private final int MAX_DEPTH = 2;
 	
     /**
      * Performs a move
@@ -71,11 +69,11 @@ public class Player {
 
     public Vector<GameState> evalSort(Vector<GameState> states){
         Vector<M> mStates = new Vector<>();
-
+        
         for (GameState g:states){
             mStates.add(new M(g,evaluationFunction(g,g.getNextPlayer())));
         }
-        mStates.sort(Comparator.comparing(M::getEval).reversed());
+        mStates.sort(Comparator.comparing(M::getEval));
 
         return new Vector<GameState>(mStates.stream().map(M::getGameState).collect(Collectors.toList()));
 
@@ -93,9 +91,11 @@ public class Player {
         gameState.findPossibleMoves(nextStates);
         nextStates = evalSort(nextStates);
         int v;
-
+        
         if (depth==0 || nextStates.isEmpty()){
-            v = evaluationFunction(gameState,player);
+            v = evaluationFunction(gameState,Constants.CELL_X);
+            s.add(v);
+            //return nextStates.elementAt(v);
         } else if (player==Constants.CELL_X){
             v = Integer.MIN_VALUE;
             for (GameState g: nextStates){
@@ -106,6 +106,7 @@ public class Player {
                     break;
                 }
             }
+           // return nextStates.elementAt(alpha);
         } else {
             v = Integer.MAX_VALUE;
             for (GameState g: nextStates){
@@ -116,9 +117,11 @@ public class Player {
                     break;
                 }
             }
+            //return nextStates.elementAt(beta);
 
         }
 
+     // 
         return nextStates.elementAt(s.indexOf(Collections.max(s)));
 
     }
@@ -128,46 +131,48 @@ public class Player {
         Vector<GameState> nextStates = new Vector<GameState>();
         gameState.findPossibleMoves(nextStates);
         if (depth>0) {
-            nextStates = evalSort(nextStates);
+        	nextStates = evalSort(nextStates);
         }
         int v;
 
         if (depth==0 || nextStates.isEmpty()){
-            v = evaluationFunction(gameState,player);
+            v = evaluationFunction(gameState,Constants.CELL_X);
+            return v;
         } else if (player==Constants.CELL_X){
             v = Integer.MIN_VALUE;
             for (GameState g: nextStates){
                 v = Math.max(v, alphabeta(g,depth-1,alpha,beta,Constants.CELL_O));
                 alpha = Math.max(alpha,v);
                 if (beta <= alpha){
-                    break;
+                	return alpha;
+                   // break;
                 }
             }
+            return alpha;
         } else {
             v = Integer.MAX_VALUE;
             for (GameState g: nextStates){
                 v = Math.min(v, alphabeta(g,depth-1,alpha,beta,Constants.CELL_X));
                 beta = Math.min(beta,v);
                 if (beta <= alpha){
-                    break;
+                	return beta;
+                    //break;
                 }
             }
-
+            return beta;
         }
-        return v;
+  //      return v;
 
     }
 
    public int evaluationFunction(GameState state, int player){
 	   
-       if (state.isXWin()){
-           return 1000;
-       } else if (state.isOWin()){
-           return -1000;
-       } else if (state.isEOG()){
-           return 0;
-       }
        int thisStatesScore = 0;
+       
+       if(state.isXWin()) {
+    	   thisStatesScore += 100000000; 
+       }
+      
        
        
        //for(int num = 0; num<GameState.CELL_COUNT; num++) {
@@ -183,46 +188,53 @@ public class Player {
        for(int layer = 0; layer<GameState.BOARD_SIZE; layer++) {
            // Sum all points for rows in each layer
            for(int row = 0; row<GameState.BOARD_SIZE; row++){
-               thisStatesScore += myMarks(state, row,row,0,GameState.BOARD_SIZE,layer,layer,player);
+               thisStatesScore += myMarks(state, row, row, 0, GameState.BOARD_SIZE-1, layer, layer, player);
            }
            for(int col = 0; col<GameState.BOARD_SIZE; col++){
-	           thisStatesScore += myMarks(state, 0,GameState.BOARD_SIZE,col,col,layer,layer,player);
+	           thisStatesScore += myMarks(state, 0, GameState.BOARD_SIZE-1, col, col, layer, layer, player);
 	       }
            thisStatesScore += myMarks(state, 0,GameState.BOARD_SIZE-1,GameState.BOARD_SIZE-1,0,layer,layer,player);
            thisStatesScore += myMarks(state, 0,GameState.BOARD_SIZE-1,0,GameState.BOARD_SIZE-1,layer,layer,player);
+           //thisStatesScore += myMarks(state, GameState.BOARD_SIZE-1,0,GameState.BOARD_SIZE-1,0,layer,layer,player);
+           //thisStatesScore += myMarks(state, GameState.BOARD_SIZE-1,0,0,GameState.BOARD_SIZE-1,layer,layer,player);
        }
        
        // up from the y-axis
        for(int row = 0; row<GameState.BOARD_SIZE; row ++) {
     	   // sum all points for each layer
     	   for(int layer = 0; layer<GameState.BOARD_SIZE; layer++) {
-               thisStatesScore += myMarks(state, row, row, 0, GameState.BOARD_SIZE, layer, layer, player);
+               thisStatesScore += myMarks(state, row, row, 0, GameState.BOARD_SIZE-1, layer, layer, player);
     	   }
     	   for(int col = 0; col<GameState.BOARD_SIZE; col++) {
-    		   thisStatesScore += myMarks(state, row, row, col, col, 0, GameState.BOARD_SIZE, player);
+    		   thisStatesScore += myMarks(state, row, row, col, col, 0, GameState.BOARD_SIZE-1, player);
     	   }
            thisStatesScore += myMarks(state, row, row, 0, GameState.BOARD_SIZE-1, 0, GameState.BOARD_SIZE-1,player);
            thisStatesScore += myMarks(state, row, row, 0, GameState.BOARD_SIZE-1, GameState.BOARD_SIZE-1, 0,player);
+         //  thisStatesScore += myMarks(state, row, row, GameState.BOARD_SIZE-1, 0, 0, GameState.BOARD_SIZE-1,player);
+          // thisStatesScore += myMarks(state, row, row, GameState.BOARD_SIZE-1, 0, GameState.BOARD_SIZE-1, 0,player);
        }
        
        // up from the x-axis
        for(int col = 0; col<GameState.BOARD_SIZE; col ++) {
     	   // all rows
     	   for(int row = 0; row<GameState.BOARD_SIZE; row++) {
-               thisStatesScore += myMarks(state, row, row, col, col, 0, GameState.BOARD_SIZE, player);
+               thisStatesScore += myMarks(state, row, row, col, col, 0, GameState.BOARD_SIZE-1, player);
     	   }
     	   // layers
     	   for(int layer = 0; layer<GameState.BOARD_SIZE; layer++) {
-    		   thisStatesScore += myMarks(state, 0, GameState.BOARD_SIZE, col, col, layer, layer, player);
+    		   thisStatesScore += myMarks(state, 0, GameState.BOARD_SIZE-1, col, col, layer, layer, player);
     	   }
            thisStatesScore += myMarks(state, 0,GameState.BOARD_SIZE-1,col,col,0,GameState.BOARD_SIZE-1,player);
            thisStatesScore += myMarks(state, 0,GameState.BOARD_SIZE-1,col,col,GameState.BOARD_SIZE-1,0,player);
+         //  thisStatesScore += myMarks(state, GameState.BOARD_SIZE-1, 0, col,col,0,GameState.BOARD_SIZE-1,player);
+          // thisStatesScore += myMarks(state, GameState.BOARD_SIZE-1, 0,col,col,GameState.BOARD_SIZE-1,0,player);
        }
        
        // the four different diagonal lines that go from (0,0,0) to (max x, max y, max z)
        // , (0,max y, 0) to (max x, 0, max z), (max x, 0, 0) to (0, max y, max z)
        // and lastly from (max x, max y, 0) to (0, 0, max z)
        // 0,0,0
+       
        thisStatesScore += myMarks(state, 0, GameState.BOARD_SIZE-1,0,GameState.BOARD_SIZE-1,0,GameState.BOARD_SIZE-1,player);
        // max y
        thisStatesScore += myMarks(state, GameState.BOARD_SIZE-1, 0, 0, GameState.BOARD_SIZE-1, 0,GameState.BOARD_SIZE-1,player);
@@ -230,7 +242,7 @@ public class Player {
        thisStatesScore += myMarks(state, 0, GameState.BOARD_SIZE-1, GameState.BOARD_SIZE-1, 0, 0, GameState.BOARD_SIZE-1,player);
        // max x max y
        thisStatesScore += myMarks(state, GameState.BOARD_SIZE-1, 0, GameState.BOARD_SIZE-1,0,0,GameState.BOARD_SIZE-1,player);
-         
+       
        return thisStatesScore;
    }
    
@@ -238,51 +250,48 @@ public class Player {
        int XinARow = 0;
        int OinARow = 0;
        int maxX = 0;
+       int linkedCounter = 0;
        int maxO = 0;
        int emptySpaces = 0;
        int dRow = (row2 - row) / (GameState.BOARD_SIZE - 1);
        int dCol = (col2 - col) / (GameState.BOARD_SIZE - 1);
        int dLayer = (layer2 - layer) / (GameState.BOARD_SIZE -1);
-       int oldPiece = state.at(row, col, layer);
-       if (oldPiece == Constants.CELL_X) {
-           XinARow++;
-       } else if (oldPiece == Constants.CELL_O) {
-           OinARow++;
-       } else {
-           emptySpaces++;
-       }
-       for (int i = 1; i < GameState.BOARD_SIZE; i++) {
+       int sum = 0;
+       int oldPiece = 0;
+       for (int i = 0; i < GameState.BOARD_SIZE; i++) {
            int piece = state.at(row + i * dRow, col + i * dCol, layer + i*dLayer);
-           if (piece == oldPiece) {
-               if (piece == Constants.CELL_X) {
+           if (piece == Constants.CELL_X) {
                    XinARow++;
-               } else if (piece == Constants.CELL_O) {
-                   OinARow++;
-               } else {
-                   emptySpaces++;
-               }
-           } else if (piece != oldPiece) {
-               maxX = Math.max(XinARow, maxX);
-               maxO = Math.max(OinARow, maxO);
-               XinARow = 0;
-               OinARow = 0;
-               if (piece == Constants.CELL_EMPTY) {
-                   emptySpaces++;
-               } else if (piece == Constants.CELL_X) {
-                   XinARow++;
-               } else {
-                   OinARow++;
-               }
-           }
-           oldPiece = piece;
+                   if(oldPiece == piece) {
+                	   linkedCounter += 1;
+                   }
+                   oldPiece = piece;
+        	   sum += 10;
+           } 
        }
-       if (emptySpaces == 0) {
-           return 0;
-       } else if (maxO*maxX == 0){
-           return Xweights[maxX] - Oweights[maxO];
-       } else {
-           return 0;
+       
+       if(XinARow>1) {
+    	   sum += 50;
        }
+       if(XinARow>2) {
+    	   sum += 500;
+       }
+       if(XinARow>3) {
+    	   sum += 5000;
+       }
+       if(linkedCounter>0) {
+    	   sum += 500;
+       }
+       if(linkedCounter>1) {
+    	   sum += 5000;
+       }
+       if(linkedCounter>2) {
+    	   sum += 10000;
+       }
+       if(linkedCounter>3) {
+    	   sum += 100000;
+       }
+       return sum;
    }
     
 }
